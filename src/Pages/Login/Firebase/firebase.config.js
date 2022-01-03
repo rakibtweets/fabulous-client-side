830e6b467ece61828c92a9e13e3c082f5ce206e2
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-03 19:07:36 +0600

    firebase configuration

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAtc4ZNvUtgmluOHV6UOvAcld-yPmrqm6Q',
+  authDomain: 'fabulous-f5a4b.firebaseapp.com',
+  projectId: 'fabulous-f5a4b',
+  storageBucket: 'fabulous-f5a4b.appspot.com',
+  messagingSenderId: '1030877939257',
+  appId: '1:1030877939257:web:2b490d0cb61232fa3e2fe8',
+};
+
+export default firebaseConfig;
