612edd79dc651d1b3caa0e56e8720e0c351cc813
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-04 00:17:05 +0600

    react hook form login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
