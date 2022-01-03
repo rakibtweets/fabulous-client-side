9ed32912d3a9a24a21e716c24e0487fd6d85c481
Ariful01303 <arifhossen01303@gmail.com>
2022-01-04 00:04:34 +0600

    added product

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AddedService from '../../AddedService/AddedService';
 
 const Home = () => {
   return (
     <div>
       <h2>Home page</h2>
+      <AddedService/>
     </div>
   );
 };
