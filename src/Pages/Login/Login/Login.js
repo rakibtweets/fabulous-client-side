cb3cc815d5bdb44a80f58dae02b16394651cab4a
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-04 19:27:38 +0600

    firebase complete

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Spinner } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import './Login.css';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '../../../Hooks/useAuth';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { signWithGoogle, loginUser, authError, isLoading } = useAuth();
+  const { signWithGoogle, loginUser, isLoading } = useAuth();
+  const handleGoogleSignIn = () => {
+    signWithGoogle(location, navigate);
+  };
   const {
     register,
     handleSubmit,
@@ -24,41 +28,59 @@ const Login = () => {
       <Container>
         <Card className="p-5 w-50 mx-auto mt-5">
           <h3 className="fw-bold mb-3">Login</h3>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              className="form-control rounded-pill"
-              placeholder="Email"
-              type="email"
-              {...register('email', { required: true })}
+          {isLoading ? (
+            <Spinner
+              className="container-fluid text-center"
+              animation="border"
+              variant="danger"
+              size="6em"
             />
-            <br />
+          ) : (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="form-control rounded-pill"
+                placeholder="Email"
+                type="email"
+                {...register('email', { required: true })}
+              />
+              <br />
 
-            <input
-              className="form-control rounded-pill"
-              placeholder="Password"
-              type="password"
-              {...register('password', { required: true })}
-            />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
-            <br />
+              <input
+                className="form-control rounded-pill"
+                placeholder="Password"
+                type="password"
+                {...register('password', { required: true })}
+              />
+              {/* errors will return when field validation fails  */}
+              {errors.exampleRequired && <span>This field is required</span>}
+              <br />
 
+              <button
+                className="btn login__btn text-uppercase rounded-pill w-100"
+                type="submit"
+              >
+                Sign In
+              </button>
+              <br />
+              <br />
+
+              <Link
+                className="text-decoration-none text-secondary"
+                to="/register"
+              >
+                Create Account
+              </Link>
+            </form>
+          )}
+          <div>
+            <h3 className="fw-bold text-secondary mx-auto my-3">OR</h3>
             <button
-              className="btn login__btn text-uppercase rounded-pill w-100"
-              type="submit"
+              onClick={handleGoogleSignIn}
+              className="btn google-btn py-2 w-100 rounded-pill"
             >
-              Sign In
+              <FcGoogle className="fs-3 me-2" /> Sign In With Google
             </button>
-            <br />
-            <br />
-
-            <Link
-              className="text-decoration-none text-secondary"
-              to="/register"
-            >
-              Create Account
-            </Link>
-          </form>
+          </div>
         </Card>
       </Container>
     </>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import './Register.css';
+import { Card, Container, Spinner } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import swal from 'sweetalert';
 import useAuth from '../../../Hooks/useAuth';
+import { FcGoogle } from 'react-icons/fc';
 
 const Register = () => {
   const navigate = useNavigate();
-  const { signWithGoogle, registerNewUser, authError, isLoading } = useAuth();
+  const location = useLocation();
+  const { signWithGoogle, registerNewUser, isLoading } = useAuth();
+  const handleGoogleSignIn = () => {
+    signWithGoogle(location, navigate);
+  };
   const {
     register,
     handleSubmit,
@@ -29,52 +35,72 @@ const Register = () => {
       <Container>
         <Card className="p-5 w-50 mx-auto mt-5">
           <h3 className="fw-bold mb-3">Create Account</h3>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              className="form-control rounded-pill"
-              placeholder="Name"
-              type="name"
-              {...register('name', { required: true })}
-            />
-            <br />
-            <input
-              className="form-control rounded-pill"
-              placeholder="Email"
-              type="email"
-              {...register('email', { required: true })}
-            />
-            <br />
 
-            <input
-              className="form-control rounded-pill"
-              placeholder="Password"
-              type="password"
-              {...register('password', { required: true })}
+          {isLoading ? (
+            <Spinner
+              className="container-fluid text-center"
+              animation="border"
+              variant="danger"
+              size="6em"
             />
-            {/* errors will return when field validation fails  */}
-            {errors.password && <span>This field is required</span>}
-            <br />
-            <input
-              className="form-control rounded-pill"
-              placeholder="Confirm Password"
-              type="password"
-              {...register('confirmPassword', { required: true })}
-            />
-            <br />
+          ) : (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="form-control rounded-pill"
+                placeholder="Name"
+                type="name"
+                {...register('name', { required: true })}
+              />
+              <br />
+              <input
+                className="form-control rounded-pill"
+                placeholder="Email"
+                type="email"
+                {...register('email', { required: true })}
+              />
+              <br />
 
+              <input
+                className="form-control rounded-pill"
+                placeholder="Password"
+                type="password"
+                {...register('password', { required: true })}
+              />
+              {/* errors will return when field validation fails  */}
+              {errors.password && <span>This field is required</span>}
+              <br />
+              <input
+                className="form-control rounded-pill"
+                placeholder="Confirm Password"
+                type="password"
+                {...register('confirmPassword', { required: true })}
+              />
+              <br />
+
+              <button
+                className="btn login__btn text-uppercase rounded-pill w-100"
+                type="submit"
+              >
+                Create Account
+              </button>
+              <br />
+              <br />
+
+              <Link className="text-decoration-none text-secondary" to="/login">
+                All ready have account ?
+              </Link>
+            </form>
+          )}
+
+          <div>
+            <h3 className="fw-bold text-secondary mx-auto my-3">OR</h3>
             <button
-              className="btn login__btn text-uppercase rounded-pill w-100"
-              type="submit"
+              onClick={handleGoogleSignIn}
+              className="btn google-btn py-2 w-100 rounded-pill"
             >
-              Create Account
+              <FcGoogle className="fs-3 me-2" /> Sign In With Google
             </button>
-            <br />
-            <br />
-
-            <Link className="text-decoration-none text-secondary" to="/login">
-              All ready have account ?
-            </Link>
-          </form>
+          </div>
         </Card>
       </Container>
     </>
